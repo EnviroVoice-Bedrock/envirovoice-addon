f6a3ca9ca8765cae41e93b8178a48425d70c6cde
Halo333X <124174275+Halo333X@users.noreply.github.com>
2026-08-03 15:21:40 -0600

    Addon

---
+++ BP/scripts/Icons.js
@@ -0,0 +1,11 @@
+const icons = {
+    unmute: '',
+    mute: '',
+    deafen: '',
+    cloud: '',
+    raindrop: '',
+    cave: '',
+    buried: '',
+    talking: '',
+}
+export default icons;
